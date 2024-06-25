480335e8291410ea40e92ec896aea69de9bc29dc
Gleb <glebbus89@gmail.com>
2024-06-25 12:13:23 +0300

    Домашнее задание

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log('Текст из script.js!')
+// 
+// const num = 123;
+// console.log("num = " + num);
+// задание 2
+// let a = 5;
+// a = 7;
+// console.log(`a = ${a}`);
+// задание 2
+// const b = 1 + 2 + 3;
+// console.log('b =' + b);
+// задание 3
+
+// const c = 10;
+// const d = 2;
+// let numRaz = c - d;
+// console.log(numRaz);
+
+// let numSum = c + d;
+// console.log(numSum);
+
+// let numMult = c * d;
+// console.log(numMult);
+
+// let numDel = c / d ;
+// console.log(numDel);
+
+// console.log(c + d + numRaz + numSum + numDel + numMult);
+
+
+// Задание 5
+
+// const a = 1.5;
+// const b = 0.75;
+// let numSsum = a + b;
+// console.log(numSsum);
+
+// const c = - 1.5;
+// console.log(c * - 1);
+
+// Задание 6
+
+// const fname = "Gleb";
+// const surename = "Kuznetsov";
+// console.log(fname, surename); 
+
+// const j = "java"
+// const s = "script"
+// console.log(j+s);
+
+// const h = "hello";
+// const w = "world";
+// console.log(`${h} ${w}`);
+
+
+
+
+
+
+
+
+
+
+
+
